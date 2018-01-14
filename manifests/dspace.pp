9aa5b27165a10c333beecb3f342878c9854a4213
Billy Nelson <bilnels@users.noreply.github.com>
2018-01-14 04:25:30 -0500

    Update dspace.pp

--- manifests/dspace.pp
+++ manifests/dspace.pp
@@ -1,31 +1,18 @@
 class { 'dspace':
-  # Base settings
-  owner              => 'dspace1',  # OS user who "owns" DSpace
-  
-  # Database specific settings
-  db_name            => 'dspacepuppet1',   # Name of database to use
-  db_owner           => 'dspacepuppet',   # DB owner account info
-  db_owner_passwd    => 'dspacepuppet',
+##############################
+# Setup all DSpaceDirect sites 
+# (based on Hiera data configuration)
+##############################
+# These next few lines load the Hiera data configs and creates a new "dspace::site"
+# for every site defined under "DSpaceDirect_Sites" in the 'hieradata/[fqdn].yaml' file.
 
- 
-  # Tomcat specific settings
-  tomcat_port        => 8081,
- 
-  # DSpace installation settings
-  #git_repo           => 'git@github.com:myacccount/DSpace.git',  # Git repo to use for DSpace source
-  #git_branch         => 'dspace-5_x',  #Git branch to use
- 
-  # DSpace Admin Account settings
-  admin_firstname    => 'Isaac',
-  admin_lastname     => 'Muteti',
-  admin_email        => 'isaac.muteti@digitaldividedata.com',
-  admin_passwd       => '1234',
-  admin_language     => 'en',
+$dspacedirect_sites = hiera('DSpaceDirect_Sites', {})   # First read the site configs under "DSpaceDirect_Sites" (default to doing nothing, {}, if nothing is defined under "DSpaceDirect_Sites") 
+create_resources('dspace::site', $dspacedirect_sites)   # Then, create a new "dspace::site" for each one
 }
 
 #owner
-dspace::owner { 'dspace1':
-  gid    => 'dspace1',  # Primary OS group name / ID
+dspace::owner { '$owner':
+  #gid    => 'dspace1',  # Primary OS group name / ID
   groups => 'root', # Additional OS groups
   sudoer => true,  # Whether to add acct as a sudoer
 }
