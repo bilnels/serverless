aae52d8b968e82229c46b9ce3dd5b7c79f324ec5
Billy Nelson <bilnels@users.noreply.github.com>
2018-01-14 09:17:41 -0500

    Update dspace.pp

--- manifests/dspace.pp
+++ manifests/dspace.pp
@@ -1,16 +1,20 @@
 hiera_include('classes')
-class { 'dspace':
+
+# DSpace needs prerequisites before it can be installed, so 'server' class needs loading first
+Class['server'] -> Class['dspace']
+
 ##############################
 # Setup all DSpaceDirect sites 
 # (based on Hiera data configuration)
 ##############################
 # These next few lines load the Hiera data configs and creates a new "dspace::site"
 # for every site defined under "DSpaceDirect_Sites" in the 'hieradata/[fqdn].yaml' file.
-
 # Create a hash from Hiera Data with variable values
 $dspacedirect_sites = hiera('DSpaceDirect_Sites', {})   # First read the site configs under "DSpaceDirect_Sites" (default to doing nothing, {}, if nothing is defined under "DSpaceDirect_Sites") 
 create_resources('dspace::site', $dspacedirect_sites)   # Then, create a new "dspace::site" for each one
-}
+
+
+class { 'dspace':}
 
 #owner
 dspace::owner { '$owner':
